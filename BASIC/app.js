845e8a076f35e0db95ca8dfa0ebb129e1ffe72b1
rahulteli16 <rahulmodimodi740@gmail.com>
2025-06-17 23:15:58 +0530

    Add files via upload

---
+++ BASIC/app.js
@@ -0,0 +1,47 @@
+ // console.log("JavaScript!");
+
+// let a = 45;
+// let b = 45;
+// console.log("sum :", a+b);
+
+
+// let pencilprice = 10;
+// let eraser = 5;
+// // let output = "total price is :"  + (pencilprice + eraser) + " rupees."
+// let output = `The Total price is ${pencilprice + eraser} rupees`
+// console.log(output);
+
+
+
+
+// alert("hey modi rahul");
+// console.log("welcome rahul");
+// console.error("somthing got worng");
+// console.warn("last time warn")
+
+
+let fisrtname = prompt("Enter your first name");
+let lastname = prompt("Enter your last name");
+let msg = fisrtname+ " " + lastname;
+console.log(msg);
+alert(msg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
