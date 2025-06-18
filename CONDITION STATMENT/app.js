9ea6787cbbdbf796fc50ad4592b48eaedef1d0f1
rahulteli16 <rahulmodimodi740@gmail.com>
2025-06-18 23:35:27 +0530

    Add files via upload

---
+++ CONDITION STATMENT/app.js
@@ -0,0 +1,181 @@
+// if statment  (Example-1)
+
+// let age = 21;
+// if (age >= 18){
+// console.log("you can vote")
+// }
+// if (age >= 21){
+// console.log("You are meture")
+// }
+// if (age < 18){
+//     console.log("you cannot vote ")
+// }
+
+
+
+// //Example-2
+
+// let firstname = "rahul";
+// if (firstname == "rahul")
+//     console.log(`welcome ${firstname}`)
+
+
+// if else
+ 
+// let myage = 10;
+
+// if (myage > 18){
+//     console.log("adult")
+// }  
+// else if (myage == 18){
+//     console.log("medium")
+// }
+// else if (myage == 10){
+//     console.log("start")
+// }
+// else {
+// console.log("biginer")
+// }
+
+
+
+//prectise (create trafic light system)
+
+// let light = "blue";
+
+// if (light === "red"){
+//     console.log("stop! light is red")
+// }
+// else if (light === "yellow"){
+//     console.log("Look! light is yellow")
+// }
+// else if (light === "green"){
+//     console.log("Go! light is green")
+// }
+// else {
+//     console.log("light is broken")
+// }
+
+
+    
+// school marks grading
+
+// let marks = 80;
+
+// if (marks >= 80){
+//     console.log("A+")
+// } 
+// else if (marks >= 60){
+//     console.log("A")
+// }
+// else if (marks >= 33){
+//     console.log("B")
+// }
+// else if (marks < 33){
+//     console.log("Fail")
+// }
+
+
+// wether Ditected
+
+// let month = "april"
+
+// if (month == "january"){
+//     console.log("winter")
+// }
+// else if (month = "april"){
+//     console.log("summer")
+// }
+
+
+
+//Popkon price calculate acording size (S=50 , M=100 , L=200 , XL=250)
+
+// let size = "l";
+
+// if (size == "s"){
+//     console.log("popkon price is : 50");
+// }
+// else if (size == "m"){
+//     console.log("popkon price is : 100");
+// }
+// else if (size == "l"){
+//     console.log("popkon price is : 200");
+// }
+// else if (size == "xl"){
+//     console.log("popkon price is : 250");
+// }
+// else{
+//     console.log("popkon price is : 500");
+// }
+
+
+//nested if-else
+
+// let num = 55;
+
+// if (num >= 40){
+// console.log("pass");
+//     if (num >= 80)
+//     console.log("O")
+//     if (num >= 60)
+//     console.log("A+")
+//     else
+//     console.log("B")
+// }
+// else{
+//     console.log("better luck next time");
+// }
+
+
+// switch statment
+
+// let color = "green"
+
+// switch(color){
+//  case "red":
+//  console.log("stop")
+//  break;
+
+//  case "green":
+//  console.log("go")
+//  break;
+
+//  case "yellow":
+//  console.log("look")
+//  break;
+
+//  default:
+// console.log("light broken")
+// }
+
+
+//prectise  Qs
+
+let day = 5;
+
+switch (day){
+    case 1 :
+        console.log("sunday")
+        break;
+    case 2 :
+        console.log("monday")
+        break;
+    case 3 :
+        console.log("tuesday")
+        break;
+    case 4 :
+        console.log("wensday")
+        break;
+    case 5 :
+        console.log("thurday")
+        break;
+    case 6 :
+        console.log("friday")
+        break;
+    case 7 :
+        console.log("saturday")
+        break;
+    }
+
+    
